73cb4296bac861c88e62e658c7c1a554e480a374
Lakshya-Playzz <85876093+Lakshya-Playzz@users.noreply.github.com>
2022-05-25 15:29:35 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,45 @@
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_coin  = loadSound("coin.wav")
+	game_over = loadSound("gameover.wav")
+	mario_jump = loadSound("jump.wav")
+	mario_kick = loadSound("kick.wav")
+	mario_die = loadSound("mariodie.wav")
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	video = createCapture(VIDEO);
+	video.size(800,400)
+	canvas.parent('game_canvas')
+	video.parent('game_console')
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose' , gotPoses);
+	
+
+}
+function modelLoaded(){
+	console.log("Model Is Loaded")
+}
+function gotPoses(results){
+	if(results.length > 0){
+		console.log(results)
+		 noseX = results[0].pose.nose.x 
+		 noseY = results[0].pose.nose.y
+		 
+	}
+}
+
+
+function draw() {
+	game()
+}
+
+
+
+
+
+
